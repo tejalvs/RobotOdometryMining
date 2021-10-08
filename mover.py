e0c8bbfe1f8ae9ed2d0f30bb24bd81058bfd0658
tejalvs <tejalvs14@gmail.com>
2021-10-08 10:24:45 -0400

    Revert "Initial Position retrieval"
    This reverts commit 9ee36433b7f3a088f1385a06afcc5162dee49c8f.

--- mover.py
+++ mover.py
@@ -38,7 +38,6 @@ class mover():
 		self.startt = None
 		self.mspub = rospy.Publisher('/cmd_motor_state', MotorState, queue_size = 1,latch=True)
 		self.mspub.publish(1)
-		self.initial_position=None
 	
 
 	def odom_callback(self, odom):
@@ -114,19 +113,13 @@ class mover():
 		data_odom=None
 		while data_odom is None:
 			try :
-				data_odom=rospy.wait_for_messages("/pose",Odometry, timeout=1)
-				data_range=rospy.wait_for_messages("/scan",LaserScan, timeout=1)
+				data_odom=rospy.wait_for_messages("/odom",Odometry, timeout=1)
 			except:
 				rospy.loginfo("Current odom not ready yet, retrying  for setting up init position")
-		self.initial_position=Pose()
-		self.initial_position.x=data_odom.pose.pose.position.x
-		self.initial_position.y=data_odom.pose.pose.position.y
-		qz = odom.pose.pose.orientation.z
-        qw = odom.pose.pose.orientation.w
-        self.initial_position.theta = atan2(2 * qw * qz, 1 - 2 * qz * qz)
-
-
-
+		self.start_position=Point()
+		self.start_position=data_odom.pose.pose.position.x
+		self.start_position=data_odom.pose.pose.position.y
+		print()
 		
 			
 
@@ -139,15 +132,11 @@ def main():
      v = float(sys.argv[2])
      w = float(sys.argv[3])
      mv = mover(t,v,w)
-     mv.get_init_position()
-     print("x=",mv.initial_position.x)
-     print("y=",mv.initial_position.y)
-     rospy.loginfo("theta=",mv.initial_position.theta)
-#      rospy.Subscriber("/pose", Odometry, mv.odom_callback)
-#      rospy.Subscriber("/scan", LaserScan, mv.scan_callback)
+     rospy.Subscriber("/pose", Odometry, mv.odom_callback)
+     rospy.Subscriber("/scan", LaserScan, mv.scan_callback)
      #rospy.Subscriber("/camera/rgb/image_color", Image, mv.image_callback)
      #rospy.Timer(rospy.Duration(0.1), mv.timer_callback)
-#      rospy.spin()
+     rospy.spin()
      
 main()
 
