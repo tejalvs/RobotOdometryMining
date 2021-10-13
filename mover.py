2a1e83dfe27f8a01021401389e30bf51cbeaf656
tejalvs <tejalvs14@gmail.com>
2021-10-12 21:24:55 -0400

    added x,y dicionary changes

--- mover.py
+++ mover.py
@@ -12,10 +12,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 class Pose():
 	def __init__(self):
-    	self.header = None
-    	self.x = 0
-    	self.y = 0
-    	self.theta = 0
+	    	self.header = None
+	    	self.x = 0
+	    	self.y = 0
+	    	self.theta = 0
 
 
 def odom_to_pose(odom):
@@ -30,116 +30,92 @@ def odom_to_pose(odom):
 	return pose
 
 class mover():
-    def __init__(self, t, v, omega):
-   	 self.vel = v
-   	 self.omega = omega
-   	 self.tm = t
-   	 self.vpub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
-   	 self.startt = None
-   	 self.mspub = rospy.Publisher('/cmd_motor_state', MotorState, queue_size = 1,latch=True)
-   	 self.mspub.publish(1)
-   	 self.initial_position=None
-   	 self.counter=0
-   	 self.initial_range=None
+	def __init__(self, t, v, omega):
+		self.vel = v
+		self.omega = omega
+		self.tm = t
+		self.vpub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+		self.startt = None
+		self.mspub = rospy.Publisher('/cmd_motor_state', MotorState, queue_size = 1,latch=True)
+		self.mspub.publish(1)
+		self.initial_position=None
+		self.counter=0
+		self.initial_range=None
 
 
-    def odom_callback(self, odom):
-   	 if self.counter==0:
-   	 	self.initial_position = odom_to_pose(odom)
-   	 	self.counter+=1
-   	 	ogdata="x="+str(self.initial_position.x)+" y="+str(self.initial_position.y)+" theta="+str(self.initial_position.theta)
-   	 	rospy.loginfo("initial Pose information(x,y, theta, v, omega) %s",ogdata)
-   	 self.pose = odom_to_pose(odom)
-   	 logdata="x="+str(self.pose.x)+" y="+str(self.pose.y)+" theta="+str(self.pose.theta)+"velocity="+str(self.vel)+" omega="+str(self.omega)
-   	 rospy.loginfo("Pose information(x,y, theta, v, omega) %s",logdata)
+	def odom_callback(self, odom):
+		if self.counter==0:
+			self.initial_position = odom_to_pose(odom)
+			self.counter+=1
+			init_data="x="+str(self.initial_position.x)+" y="+str(self.initial_position.y)+" theta="+str(self.initial_position.theta)
+			rospy.loginfo("initial Pose information(x,y, theta, v, omega) %s",init_data)
+		self.pose = odom_to_pose(odom)
+		logdata="x="+str(self.pose.x)+" y="+str(self.pose.y)+" theta="+str(self.pose.theta)+"velocity="+str(self.vel)+" omega="+str(self.omega)
+		rospy.loginfo("Pose information(x,y, theta, v, omega) %s",logdata)
+
+	def get_range_coordinates(self,range_data,angle_min,angle_max,angle_increment,delta):
+		x={}
+		y={}
+		for i in range(len(range_data)):
+			if isnan(range_data[i]):
+				continue
+			else:
+				alpha=angle_min+(i*angle_increment)
+				x[alpha]=(self.initial_position.x+cos(self.initial_position.theta+alpha))*range_data[i]
+				y[alpha]=(self.initial_position.y+sin(self.initial_position.theta+alpha))*range_data[i]
+		return x,y
 
 
-    def get_range_coordinates(self,range_data,angle_min,angle_max,angle_increment):
-   	 x=[]
-   	 y=[]
-   	 for i in range(len(range_data)):
-   		 if isnan(range_data[i]):
-   		 #rospy.loginfo("True")
-   			 continue
-   		 else:
-   			 alpha=angle_min+(i*angle_increment)
-   			 x_new=(self.initial_position.x+cos(self.initial_position.theta+alpha))*100
-   			 y_new=(self.initial_position.y+sin(self.initial_position.theta+alpha))*100
-   			 x.append(x_new)
-   			 y.append(y_new)
-   	 x.append(self.initial_position.x)
-   	 y.append(self.initial_position.y)
-   	 return x,y
-
-    def scan_callback(self, scan):
-   	 range_data=scan.ranges
-   	 angle_min=scan.angle_min
-   	 angle_max=scan.angle_max
-   	 angle_increment=scan.angle_increment
-   	 x,y=self.get_range_coordinates(range_data,angle_min,angle_max,angle_increment)
-   	 if self.counter==1:
-   		 plt.xlim(-100, 110)
-   		 plt.ylim(-100, 110)
-   		 plt.scatter(x,y)
-   		 plt.show()
-   		 self.counter+=1
+	def scan_callback(self, scan):
+		range_data=scan.ranges
+		angle_min=scan.angle_min
+		angle_max=scan.angle_max
+		angle_increment=scan.angle_increment
+		if self.counter==1:
+			 x,y=self.get_range_coordinates(range_data,angle_min,angle_max,angle_increment,0)
+# 			 line_x=np.array([self.initial_position.x,x[len(x)//2]])
+# 			 line_y=np.array([self.initial_position.y,y[len(y)//2]])
+# 			 plt.xlim((int(min(x)-10)), (int(max(x)+10)))
+# 			 plt.ylim((int(min(y)-10)), (int(max(y)+10)))
+# 			 plt.scatter(x,y,linestyle='--', marker='o', color='b')
+# 			 plt.scatter((self.initial_position.x),(self.initial_position.y),10,color="red")
+# 			 rospy.loginfo(str(x_max)+","+str(y_max))
+# 			 plt.scatter((x[len(x)//2]),y[len(y)//2],10,color="red")
+# 			 plt.plot(line_x,line_y)
+# 			 plt.savefig('/home/driver/catkin_ws/src/mover/src/initial_position.png')
+# 			 plt.show()
+			 self.counter+=1
+		else:
+		    x,y=self.get_range_coordinates(range_data,angle_min,angle_max,angle_increment,0)
 
 
 
+	def timer_callback(self, event):
+		if self.startt is None:
+		    self.startt = rospy.get_rostime()
+		dt = rospy.get_rostime() - self.startt
+		if dt.secs >= self.tm:
+		    self.vpub.publish(Twist())
+		    sys.exit()
+		# if the velocity is constant, can make this in constructor
+		# but we may want more complex things, so...
+		tw = Twist()
+		tw.linear.x = self.vel
+		tw.angular.z = self.omega
+		self.vpub.publish(tw)
 
-    def image_callback(self, msg):
-   	 bridge=CvBridge()
-   	 rospy.loginfo("image information %s")
-   	 try:
-   		 cv_img=bridge.imgmsg_to_cv2(msg,"bgr8")
-   		 cv_img=(255-cv_img)
-   		 thresh=cv2.Canny(cv_img,5,150,5)
-   		 lines=cv2.HoughLinesP(thresh,2,np.pi/180,100,100,1)
-   		 rospy.loginfo(lines)
-   		 for i in range(lines.shape[0]):
-   			 for x1,y1,x2,y2 in lines[i]:
-   				 cv2.line(cv_img,(x1,y1),(x2,y2),(0,255,0),2)
-   	 except CvBridgeError:
-   		 print(e)
-   	 cv2.imwrite("image.jpeg",lines)
-   	 cv2.waitKey(3)
-
-    def timer_callback(self, event):
-   	 if self.startt is None:
-   		 self.startt = rospy.get_rostime()
-   	 dt = rospy.get_rostime() - self.startt
-   	 if dt.secs >= self.tm:
-   		 self.vpub.publish(Twist())
-   		 sys.exit()
-   	 # if the velocity is constant, can make this in constructor
-   	 # but we may want more complex things, so...
-   	 tw = Twist()
-   	 tw.linear.x = self.vel
-   	 tw.angular.z = self.omega
-   	 self.vpub.publish(tw)
-
-    def get_init_position(self):
-   	 data_odom=None
-   	 while data_odom is None:
-   		 try :
-   			 data_odom=rospy.wait_for_messages("/odom",Odometry, timeout=1)
-   		 except:
-   			 rospy.loginfo("Current odom not ready yet, retrying  for setting up init position")
-   	 self.start_position=Point()
-   	 self.start_position=data_odom.pose.pose.position.x
-   	 self.start_position=data_odom.pose.pose.position.y
 
 def main():
- 	rospy.init_node("mover")
- 	# arguments are time, fwd vel, angular vel
- 	t = float(sys.argv[1])
- 	v = float(sys.argv[2])
- 	w = float(sys.argv[3])
- 	mv = mover(t,v,w)
- 	rospy.Subscriber("/pose", Odometry, mv.odom_callback)
- 	rospy.Subscriber("/scan", LaserScan, mv.scan_callback)
-#  	rospy.Subscriber("/camera/rgb/image_color", Image, mv.image_callback)
-  	rospy.Timer(rospy.Duration(0.1), mv.timer_callback)
- 	rospy.spin()
+	rospy.init_node("mover")
+	# arguments are time, fwd vel, angular vel
+	t = float(sys.argv[1])
+	v = float(sys.argv[2])
+	w = float(sys.argv[3])
+	mv = mover(t,v,w)
+	rospy.Subscriber("/pose", Odometry, mv.odom_callback)
+	rospy.Subscriber("/scan", LaserScan, mv.scan_callback)
+	rospy.Timer(rospy.Duration(0.1), mv.timer_callback)
+	rospy.spin()
 
 main()
+
